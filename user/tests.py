b0039699352299300d32053690310a4be15966e1
SimranDembla <SimranDembla>
2020-08-16 15:17:57 +0000

    test2.3

--- user/tests.py
+++ user/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+#from django.test import TestCase
 
 # Create your tests here.
+
+import unittest 
+  
+class SimpleTest(unittest.TestCase): 
+  
+    # Returns True or False.  
+    def test(self):         
+        self.assertTrue(True) 
+  
+if __name__ == '__main__': 
+    unittest.main() 
